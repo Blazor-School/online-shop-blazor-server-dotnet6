8361e02e776dc2a36863a919d60edcfa2d1fc3ae
Phuoc Huy Tran <huytp@mti-fintech.info>
2021-12-12 21:33:42 +0700

    Added checkout and product list page.

--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -1,8 +1,11 @@
+using OnlineShop.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<CartService>();
 
 var app = builder.Build();
 
